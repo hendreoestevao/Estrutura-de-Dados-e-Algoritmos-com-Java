8b1eb321104b1592ad829b69c09d49cea1dc8b0b
hendreo santos <hendreosantos1@outlook.com>
2026-01-07 15:11:41 -0300

    initial

---
+++ src/main/java/org/example/test/Aula02.java
@@ -0,0 +1,13 @@
+package org.example.test;
+
+import org.example.vetor.Vetor;
+
+public class Aula02 {
+    public static void main(String[] args) {
+        Vetor vetor = new Vetor(5);
+
+        int i = 0;
+        i++;
+
+    }
+}
